38b37631bff74498876a8c32f7591d7dca253bee
RazielHR <RazielHR@users.noreply.github.com>
2021-05-07 21:50:33 -0500

    labs 12 y 14

---
+++ Laboratorios/Lab10/util/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'jurasic',
+    password: ''
+    
+});
+
+module.exports = pool.promise();
